f20d5456bbf5bfa1fe39772ebfc89cf30f068300
irisgyq <gouyingqiyanxuan321@gmail.com>
2017-03-20 01:02:17 -0400

    o

--- main.go
+++ main.go
@@ -240,11 +240,11 @@ func sendGenericMessage(sender string) {
 					Buttons: &[]Button{{
 							Type: "postback",
 							Title: "Study",
-							Payload: "What kind of programming languages do you want to learn",
+							Payload: "What kind of PROGRAMMING LANGUAGES do you want to learn",
 						},{
 							Type: "postback",
 							Title: "entertainment",
-							Payload: "Let's play blackjack! If you are ready, please input 'blackjack'",
+							Payload: "Let's play BLACKJACK! If you are ready, please input 'blackjack'",
 						}},
 
 					}},
