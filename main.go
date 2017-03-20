83abe82f1d26f8e5e8f4ea50cfdba323967ef29e
irisgyq <gouyingqiyanxuan321@gmail.com>
2017-03-20 15:01:59 -0400

    j

--- main.go
+++ main.go
@@ -226,7 +226,7 @@ func sendTextMessage(sender string, text string) {
 }
 
 func sendGenericMessage(sender string, city string) {
-	resp, err := http.Get("https://graph.facebook.com/v2.6/"+city+"?fields=locale&access_token=EAARCbV0Nm8MBAKDNwgmMCBBvxZCfeql7peSivbnvkwHfboNmBVYuBVTtYFABXxp7SdB1XFqG4xtcVqg8LOMyoOctbhH6oOxgeunJ9IN7NfLYSCx9ZA6ZAGY9PEc3j1V35cB13UxkOcCuXpT91tUtyt94klpiDziQm5G5n1YagZDZD")
+	resp, err := http.Get("https://graph.facebook.com/v2.8/"+city)
 	if err!=nil {
 		//
 	}
